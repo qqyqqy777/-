e9e37d7a3a66f56ca7f63784e5a4edb1e02a3b1e
qqyqqy777 <2689354596@qq.com>
2023-02-22 21:41:14 +0800

    first commit

---
+++ src/components/Welcome.vue
@@ -0,0 +1,28 @@
+<template>
+    <!-- <img src="../assets/R-C.png" alt="" width="1200" height="600"> -->
+    <div class="welcome">
+    <h2>
+      Welcome
+    </h2>
+    <h3>
+      安捷公司商品管理系统欢迎您!
+    </h3>
+</div>
+</template>
+<script>
+export default {
+  name: 'Welcome'
+}
+</script>
+<style lang="less" scoped>
+.welcome {
+  margin: 30px auto 0;
+  font-size: 20px;
+  text-align: center;
+  color: rgb(10, 10, 10);
+  h3 {
+    margin: 50px auto 0;
+    font-size: 30px;
+  }
+}
+</style>
